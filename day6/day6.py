009391f901f3182fbd648bfe95605fa685870a96
Jonathan Croenen <jonathan.croenen@robojob.eu>
2024-12-09 18:32:36 +0100

    completed day 6

--- day6/day6.py
+++ day6/day6.py
@@ -100,11 +100,9 @@ def step(map: Map, p: Pos, d: Dir) -> tuple[Pos, Dir, bool]:
         return p, d.next(), True
     else:
         return (x + dx, y + dy), d, True
-    
-    # return (x + dx, y + dy), d, True
 
 
-def part1(map: Map, p0: Pos, d0: Dir, animate: bool) -> int:
+def simulate(map: Map, p0: Pos, d0: Dir, animate: bool) -> set:
     p = p0
     d = d0
 
@@ -119,72 +117,41 @@ def part1(map: Map, p0: Pos, d0: Dir, animate: bool) -> int:
         if not cont:
             break
 
-    return len(visited)
-
-def can_place_obstacle_in_front(map: Map, p0: Pos, p: Pos, d: Dir) -> bool:
-    width, height = len(map[0]), len(map)
-    x, y = p
-    dx, dy = d.value
-
-    if is_out_of_bounds(x + dx, y + dy, width, height):
-        return False
-
-    if map[y + dy][x + dx] == "#":
-        return False
-
-    if (x + dx, y + dy) == p0:
-        return False
-
-    return True
+    return visited
 
 
-def part2(map: Map, p0: Pos, d0: Dir, animate: bool) -> int:
-    p = p0
-    d = d0
-
-    visited = {}
-    total = 0
-    while True:
-        if animate:
-            print_map(map, p, d)
-            time.sleep(0.2)
-
-        if can_place_obstacle_in_front(map, p0, p, d):
-            visited2 = deepcopy(visited)
-            visited2.setdefault(p, set()).add(d)
-            map2 = deepcopy(map)
-
-            x, y = p
-            dx, dy = d.value
-            map2[y + dy][x + dx] = "#"
-            q, e = p, d.next()
-
-            while True:
-                if q in visited2 and e in visited2[q]:
-                    if animate:
-                        input(f"option found: ({p[0] + d.value[0]}, {p[1] + d.value[1]})")
-
-                    total += 1
-                    break
+def part1(map: Map, p0: Pos, d0: Dir, animate: bool) -> int:
+    return len(simulate(map, p0, d0, animate))
 
 
-                _q, _e, cont = step(map2, q, e)
-                if _e != e:
-                    visited2.setdefault(q, set()).update((e, _e))
-                q, e = _q, _e
+def part2(map: Map, p0: Pos, d0: Dir) -> int:
+    orig_ps = simulate(map, p0, d0, False)
+    orig_ps.remove(p0)
 
-                if not cont:
-                    break
+    result = 0
+    prev_obst = None
+    for obst_x, obst_y in orig_ps:
+        if prev_obst is not None:
+            prev_x, prev_y = prev_obst
+            map[prev_y][prev_x] = "."
 
-        _p, _d, cont = step(map, p, d)
-        if _d != d:
-            visited.setdefault(p, set()).update((d, _d))
-        p, d = _p, _d
+        map[obst_y][obst_x] = "#"
+        prev_obst = (obst_x, obst_y)
 
-        if not cont:
-            break
+        p, d = p0, d0
+        visited = set()
+        while True:
+            visited.add((p, d))
+            p, d, cont = step(map, p, d)
+            if not cont:
+                break
 
-    return total
+            if (p, d) in visited:
+                result += 1
+                break
+
+    return result
+
 
 
 def main():
@@ -193,7 +160,7 @@ def main():
     total = part1(map, p0, d0, animate=False)
     print("part 1:", total)
 
-    total = part2(map, p0, d0, animate=False)
+    total = part2(map, p0, d0)
     print("part 2:", total)
 
 
